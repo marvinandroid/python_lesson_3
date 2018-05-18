6328f085bde3a87824819c9647a5038a4278cab7
Alexander Zakharov <33317 +7 (989) 855-33-74 adzakha2@mts.ru>
2018-05-18 14:31:49 +0300

    Фронтенд переведен на Vuetify, добавлен метод /daily_data

--- logview/views.py
+++ logview/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.db import connection
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -7,3 +9,18 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'main.html')
+
+def daily_data(request):
+    with connection.cursor() as cursor:
+        cursor.execute("""
+        select
+          strftime('%Y-%m-%d', connected_at) dtick,
+          count(1) cnt
+        from logview_logrecord
+          where datetime(connected_at) >= datetime('now', '-6 months')
+        group by 1
+        order by 1;
+        """)
+        data = [{'date': x[0], 'count': x[1]} for x in cursor.fetchall()]
+    return JsonResponse(data, safe=False)
+

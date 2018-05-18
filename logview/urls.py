a07f74865c85e9dfcfc3a5a9cfe0bc06f5879b8b
Alexander Zakharov <33317 +7 (989) 855-33-74 adzakha2@mts.ru>
2018-05-18 15:41:09 +0300

    Проект переведен на модульную структуру, добавлено отображение количества пользователей

--- logview/urls.py
+++ logview/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('daily_data', views.daily_data, name='daily_data')
+    path('daily_data', views.daily_data, name='daily_data'),
+    path('daily_users', views.daily_users, name='daily_users')
 ]
--- logview/views.py
+++ logview/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'main.html')
+    return render(request, 'index.html')
 
 def daily_data(request):
     with connection.cursor() as cursor:
@@ -24,3 +24,23 @@ def daily_data(request):
         data = [{'date': x[0], 'count': x[1]} for x in cursor.fetchall()]
     return JsonResponse(data, safe=False)
 
+def daily_users(request):
+    with connection.cursor() as cursor:
+        cursor.execute("""
+        with unique_users as (
+          select distinct
+            strftime('%Y-%m-%d', connected_at) day,
+            username
+          from logview_logrecord
+            where datetime(connected_at) >= datetime('now', '-6 months')
+          order by 1
+        )
+        select
+          day,
+          count(1) cnt
+        from unique_users
+        group by day
+        order by day
+        """)
+        data = [{'date': x[0], 'count': x[1]} for x in cursor.fetchall()]
+    return JsonResponse(data, safe=False)
